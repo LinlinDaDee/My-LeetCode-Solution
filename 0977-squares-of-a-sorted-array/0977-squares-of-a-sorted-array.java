a494a877aac04b056e488262e7423d230acbd5e5
Floria <153528527+LinlinDaDee@users.noreply.github.com>
2023-12-14 13:08:09 +1100

    Time: 1 ms (100%), Space: 44.9 MB (43.99%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -7,6 +7,7 @@ class Solution {
         int[] result = new int[nums.length];
         int index = nums.length - 1;
 
+        // the next biggest square is either at the beginging or at the end, the smallest squre is in the middle
         while (left <= right){
             if (nums[left] * nums[left] > nums[right] * nums[right]){
                 result[index] = nums[left] * nums[left];
