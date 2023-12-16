0f704e79badccaefa97f9f8700dae752923ae5d1
Floria <153528527+LinlinDaDee@users.noreply.github.com>
2023-12-16 20:37:16 +1100

    Time: 0 ms (100%), Space: 41.5 MB (34.31%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -23,3 +23,23 @@ class Solution {
         return pre;
     }
 }
+
+// // 递归 
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         return reverse(null, head);
+//     }
+
+//     private ListNode reverse(ListNode prev, ListNode cur) {
+//         if (cur == null) {
+//             return prev;
+//         }
+//         ListNode temp = null;
+//         temp = cur.next;// 先保存下一个节点
+//         cur.next = prev;// 反转
+//         // 更新prev、cur位置
+//         // prev = cur;
+//         // cur = temp;
+//         return reverse(cur, temp);
+//     }
+// }
